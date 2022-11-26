df17d5d8624fd28fd7a3b4b283a358418df04a82
Smruti Ranjan Rana <smrutiranjan.developer@gmail.com>
2022-11-26 03:43:10 +0000

    [ADD] Self invoking fn

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,8 @@ import './styles.css';
 
 const app = document.getElementById('app');
 
-function init() {
-    if(!app) {
-        console.log("No app present.")
-        return
-    }
-
+(() => {
+    if(!app) return
     app.innerHTML = `<h1>Hello World!</h1>`
-}
+})()
 
-init();
